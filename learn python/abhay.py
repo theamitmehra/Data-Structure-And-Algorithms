732d10d5d4d2436aca556d52e2865b69f0e2969a
Amit <amitmehra7470@gmail.com>
2022-06-22 10:15:32 +0530

    saparated them in folders

--- learn python/abhay.py
+++
@@ -1,1 +0,0 @@
-print("abhay bhaiya")
--- learn python/cricket_score_notify.py
+++
@@ -1,5 +0,0 @@
-import random
-passlen = int(input("enter the length of password : "))
-s= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p ="".join(random.sample(s,passlen))
-print(p)
